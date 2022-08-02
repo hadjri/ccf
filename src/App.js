07297b37f522e8a47f0bb71aae20c748133a2808
malikhadjri <mjhadjri@gmail.com>
2022-08-02 02:16:47 -0400

    change html

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route}
+  from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route exact path='/' exact element={<Home />} />
+        <Route path='/registration-donations' element={<Registration/>} />
+        <Route path='/scholarships' element={<Scholarship/>} />
+      </Routes>
     </div>
   );
 }
+
+const Registration = () => {
+  return (
+
+      <div>
+        <h1>Ree</h1>
+      </div>
+  );
+};
+
+const Home = () => {
+  return (
+      <div>
+        <h1>Mail us on feedback@geeksforgeeks.org</h1>
+      </div>
+  );
+};
+
+const Scholarship = () => {
+  return (
+      <div>
+        <h1>Scholarship</h1>
+      </div>
+  );
+};
 
 export default App;
